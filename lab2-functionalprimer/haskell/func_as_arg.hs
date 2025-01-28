45ed43ba74fa32a98b6ef8e08fd3b7a91185655f
callum Thomas <220166973@aston.ac.uk>
2025-01-28 18:07:09 +0000

    task 3 and 4 completed

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,19 @@
---create inpFunc
-inpFunc = [1..5] 
+-- Define applicatorFunc with 2 arguments: inpFunc and s
+applicatorFunc :: [Int] -> Char -> Float
+applicatorFunc inpFunc s = 
+    if s == 's' 
+    then fromIntegral (sum inpFunc)  -- sum as Float
+    else fromIntegral (sum inpFunc) / 5  -- division by 5 as Float (hardcoded)
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
-
+main :: IO ()
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+    putStrLn "Enter the start number of the range:"
+    a <- readLn :: IO Int
+    putStrLn "Enter the end number of the range:"
+    b <- readLn :: IO Int  -- b is still taken from the input, but not used in the division
+
+    let inpFunc = [a..b]  -- Generate the range list from a to b
+
+    -- Call applicatorFunc with inpFunc, 'a' (or 's'), and hardcoded 5
+    let result = applicatorFunc inpFunc 'a'  -- 'a' means divide, 's' means sum
+    putStrLn ("Result: " ++ show result)  -- Display the result as a Float
